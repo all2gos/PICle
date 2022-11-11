934e696d6f7a447082066b998d2bec5ee6532fd9
all2 <stottkoraf@gmail.com>
2022-11-11 11:27:41 +0100

    p4 and p6 file have been merged to one p4 file

--- p4_mcenter_conf_bonds.py
+++ p4_mcenter_conf_bonds.py
@@ -1,6 +1,7 @@
-from p2_particle_joining import neighbor
-from p0_usable_data import cluster_cor
 import numpy as np
+
+from p0_usable_data import cluster_cor
+from p2_particle_joining import neighbor
 
 #function that returns the number of atoms in which neighborhood are n (0,1,2,3,4,5,6) atoms of nickel
 
@@ -71,15 +72,29 @@ def average_dist(topology, metal, nb_of_atom):
             avg_dist += distance(nb_of_atom,m)
     
     return avg_dist/len(metal)
+def mass_center(cluster):
+    vector_x, vector_y, vector_z, mass_sum = 0,0,0,0
+    vector = []
 
+    for atom in range(len(cluster)):
+        if cluster[atom] == 'Ni':
+            atom_mass = 58
+        else:
+            atom_mass = 63
+
+        vector_x += atom_mass*(cluster_cor[atom][0])
+        vector_y += atom_mass*(cluster_cor[atom][1])
+        vector_z += atom_mass*(cluster_cor[atom][2])
+        mass_sum += atom_mass
+    vector.append(vector_x/mass_sum)
+    vector.append(vector_y/mass_sum)
+    vector.append(vector_z/mass_sum)
     
+    return np.sqrt((vector[0]-cluster_cor[11][0])**2+(vector[1]-cluster_cor[11][1])**2+(vector[2]-cluster_cor[11][2])**2)
 
 
-    print(ni)
 if __name__ == '__main__':
-    #print(conformation_number(['Cu', 'Ni', 'Ni', 'Cu', 'Ni', 'Cu', 'Cu', 'Cu', 'Ni', 'Cu', 'Cu', 'Ni', 'Cu']))
-    #print(fully_description_of_bonds(['Cu', 'Ni', 'Ni', 'Cu', 'Ni', 'Cu', 'Cu', 'Cu', 'Ni', 'Cu', 'Cu', 'Ni', 'Cu']))
-    print(average_dist(['Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Ni', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu'], 'Ni', 1))
-   
-        
-        
+    print(mass_center(['Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni', 'Ni']))
+    print(conformation_number(['Cu', 'Ni', 'Ni', 'Cu', 'Ni', 'Cu', 'Cu', 'Cu', 'Ni', 'Cu', 'Cu', 'Ni', 'Cu']))
+    print(fully_description_of_bonds(['Cu', 'Ni', 'Ni', 'Cu', 'Ni', 'Cu', 'Cu', 'Cu', 'Ni', 'Cu', 'Cu', 'Ni', 'Cu']))
+    print(average_dist(['Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu', 'Cu'], 'Cu', 1))
