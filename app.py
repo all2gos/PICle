66662f81b2f2e38ec8a7f29504c50ef844e6db62
all2 <stottkoraf@gmail.com>
2023-01-03 14:01:20 +0100

    streamlit

--- app.py
+++ app.py
@@ -5,8 +5,37 @@ from joblib import load
 from sklearn.preprocessing import MinMaxScaler
 
 
+"""
+## Welcome to the site in which Procedure to Indicate CLusters Energy (pickle for short) has been deployed!
+
+#### You can put topological values of a cluster that you are interested in and then the machine learning model will return energy value for that cluster
+"""
+
 model = load('final_ada_m.joblib')
 
+main_menu = st.radio('You can choose the specific topological value or you can just write down topology of clusters',('Specific value','Topology'))
+
+if main_menu == 'Specific value':
+    number_of_nickel = st.slider('Choose number of nickel',0,13,0)
+    centre_atom = st.radio('Choose central atom of cluster', ('Ni','Cu'))
+    mass_center = st.number_input('Write down the lenght beetwen mass and geometry centre of cluster')
+    ni_ncentre = st.slider('Choose lenght of the shortest path throught nickel without centre atom',0,12,0)
+    ni_centre = st.slider('Choose lenght of the shortest path throught nickel with centre atom',0,12,0)
+    cu_ncentre = st.slider('Choose lenght of the shortest path throught copper without centre atom',0,12,0)
+    cu_centre = st.slider('Choose lenght of the shortest path throught copper with centre atom',0,12,0)
+    ni_0 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 0 nickel atoms', 0,12,0)
+    ni_8 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 1 nickel atoms', 0,12,0)
+    ni_17 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 2 nickel atoms', 0,12,0)
+    ni_25 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 3 nickel atoms', 0,12,0)
+    ni_33 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 4 nickel atoms', 0,12,0)
+    ni_42 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 5 nickel atoms', 0,12,0)
+    ni_50 = st.slider('Choose numbers of atom in cluster which first coordinate sphere consist of 6 nickel atoms', 0,12,0)
+    nini = st.slider('Pick the number of ni-ni bonds in cluster'0,84,0)
+    cucu = st.slider('Pick the number of cu-cu bonds in cluster'0,84,0)
+    nicu = st.slider('Pick the number of ni-cu bonds in cluster'0,84,0)
+    hydrogen = st.radio('Is cluster includes hydrogen?',('Yes','No'))
+    first_atom = st.radio('What is the first atom of the cluster to which the molecule attaches?',('Cu','Ni'))
+    second_atom = st.radio('What is the second atom of the cluster to which the molecule attaches?',('Cu','Ni'))
 
 data = pd.DataFrame(data=[[
     5,  #number of nickel in cluster 0-13
