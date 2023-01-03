e9aa7a0f7c4b637132d2bea099373581b4cfcef1
all2 <stottkoraf@gmail.com>
2023-01-03 14:08:23 +0100

    streamlit

--- app.py
+++ app.py
@@ -53,31 +53,33 @@ if main_menu == 'Specific value':
     else:
         hydrogen = 0
 
-data = pd.DataFrame(data=[[
-    5,  #number of nickel in cluster 0-13
-    1,  #c_atom, Ni = 1
-    0.04651,  #mass_center 0-0.05071
-    7,  #ni_ncentre
-    8,    #ni_centre
-    5,  #cu_ncentre
-    5,  #cu_centre
-    0,  #0ni
-    0,  #8ni
-    1,    #17ni
-    3,  #25ni
-    5,  #33ni
-    2,  #42ni
-    1,  #50ni
-    38,  #nini
-    14,  #cucu
-    32,  #nicu
-    0,  #if particle has a hydrogen then it is equall to 1
-    1,  #type_of_first_atom_ni
-    1   #type_of_second_atom_ni
-]])
-minmax = pd.DataFrame(data=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[13,1,0.05071,12,13,12,13,12,12,10,10,10,12,12,84,84,52,1,1]])
-scaler = MinMaxScaler()
-scaler.fit(minmax)
-X = scaler.transform(data)
 
-model.predict(X)[0]
+if st.button('Compute'):   
+    data = pd.DataFrame(data=[[
+        number_of_nickel,  #number of nickel in cluster 0-13
+        centre_atom,  #c_atom, Ni = 1
+        mass_center,  #mass_center 0-0.05071
+        ni_ncentre,  #ni_ncentre
+        ni_centre,    #ni_centre
+        cu_ncentre,  #cu_ncentre
+        cu_centre,  #cu_centre
+        ni_0,  #0ni
+        ni_8,  #8ni
+        ni_17,    #17ni
+        ni_25,  #25ni
+        ni_33,  #33ni
+        ni_42,  #42ni
+        ni_50,  #50ni
+        nini,  #nini
+        cucu,  #cucu
+        nicu,  #nicu
+        hydrogen,  #if particle has a hydrogen then it is equall to 1
+        first_atom,  #type_of_first_atom_ni
+        second_atom   #type_of_second_atom_ni
+    ]])
+    minmax = pd.DataFrame(data=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[13,1,0.05071,12,13,12,13,12,12,10,10,10,12,12,84,84,52,1,1]])
+    scaler = MinMaxScaler()
+    scaler.fit(minmax)
+    X = scaler.transform(data)
+
+    st.write('Energy of given cluster is:',model.predict(X)[0])
